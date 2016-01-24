69a72b156ffa4d7c42ce4626db871466a9158eb6
Evgeny Sinelnikov <sin@altlinux.ru>
2016-01-24 14:33:05 +0300

    Initial Sonar class for Ultrasonic sensor

--- ACRobot.ino
+++ ACRobot.ino
@@ -1,4 +1,5 @@
 #include "Motor.h"
+#include "Sonar.h"
 #include "Interval.h"
 
 using namespace ACRobot;
@@ -12,12 +13,18 @@ const uint8_t button = 2;
 DCMotor mA(directA, pwmA);
 DCMotor mB(directB, pwmB);
 
+const uint8_t trigSonar = 8;
+const uint8_t echoSonar = 9;
+
+Sonar sonar(trigSonar, echoSonar);
+
 Interval global(50);
 
 bool poll()
 {
   mA.poll();
   mB.poll();
+  sonar.poll();
 
   return global.poll();
 }
